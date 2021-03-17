49a543a80491354bfc7cb0531c7df16f8670c855
W-yt <lalalu9514@gmail.com>
2021-03-17 18:38:32 +0800

    添加CNN猫狗分类项目代码——数据预处理部分（刚开始）

---
+++ catdog/PreDataProcess.py
@@ -0,0 +1,12 @@
+import numpy as np
+from PIL import Image
+
+#重新命名
+def FileReName(DogType,FilePath):
+    pass
+
+#重新定义图片尺寸
+def FileResize(Output_folder,DogType,FilePath,Width = 100,Height = 100):
+    pass
+
+#读取图片返回array数据
